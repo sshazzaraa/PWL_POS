57fdf394c0ed2b41048982ead30efc9e5b0c10f6
sshazzaraa <161804393+sshazzaraa@users.noreply.github.com>
2024-03-14 19:11:44 +0700

    commit1

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,23 +10,7 @@ class UsersController extends Controller
 {
     public function index()
     {
-
-        $users = UserModel::create(
-            [
-                'username' => 'manager11',
-                'nama' => 'Manager11',
-                'password' => Hash::make('12345'),
-                'level_id' => 2,
-            ]
-        );
-
-        $users->username = 'manager12';
-
-        $users->wasChanged(); //true
-        $users->wasChanged('username'); //true
-        $users->wasChanged(['username', 'level_id']); //true
-        $users->wasChanged('nama'); //false
-        $users->wasChanged(['nama', 'username']); //true
-
+        $users = UserModel::all();
+        return view('users', ['data' => $users]);
     }
 }
