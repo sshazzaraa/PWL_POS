68587585b6d8952dd2770acc6a1c20471938763e
sshazzaraa <seshaazzaraaa@gmail.com>
2024-03-14 16:45:09 +0700

    commit1

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,7 +10,32 @@ class UsersController extends Controller
 {
     public function index()
     {
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
-        return view('user', ['data' => $user]);
+
+        $users = UserModel::create(
+            [
+                'username' => 'manager55',
+                'nama' => 'Manager55',
+                'password' => Hash::make('12345'),
+                'level_id' => 2,
+            ]
+        );
+
+        $users->username = 'manager56';
+
+        $users->isDirty(); //true
+        $users->isDirty('username'); //true
+        $users->isDirty('nama'); //false
+        $users->isDirty(['nama', 'username']); //true
+
+        $users->isClean();
+        $users->isClean('username');
+        $users->isClean('nama');
+        $users->isClean(['nama', 'username']);
+
+        $users->save();
+
+        $users->isDirty();
+        $users->isClean();
+        dd($users->isDirty());
     }
 }
