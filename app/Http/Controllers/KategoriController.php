4a577d917a3fe3ebbf5c28a7886952ca00445d9d
sshazzaraa <161804393+sshazzaraa@users.noreply.github.com>
2024-04-19 08:59:53 +0700

    Update KategoriController.php

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -2,27 +2,165 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kategori;
+use App\Models\KategoriModel;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+use Yajra\DataTables\Facades\DataTables;
 
 class KategoriController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
-      /*  $data = [
-            'kategori_kode' => 'K6',
-            'kategori_nama' => 'Canvas',
-            'created_at' => now()
+        $breadcrumb = (object)[
+            'title' => 'Daftar Kategori',
+            'list' => ['Home', 'Kategori']
         ];
-        DB::table('m_kategori')->insert($data);
-        return 'Insert data baru berhasil'; 
-        $row = DB::table('m_kategori')->where('kategori_kode', 'K6')->update(['kategori_nama' => 'kanvas']);
-        return 'Update data berhasil. Jumlah data yang diupdate : ' . $row. ' baris'; 
 
-        $row = DB::table('m_kategori')->where('kategori_kode', 'K6')->delete();
-        return 'Delete data berhasil. Jumlah data yang dihapus : ' . $row.' baris'; */
+        $page = (object)[
+            'title' => 'Daftar Kategori yang terdaftar dalam sistem'
+        ];
 
-        $data = DB::table('m_kategori')->get();
-        return view('kategori', ['data'=>$data]);
+        $activeMenu = 'kategori';
+
+        return view('kategori.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+    }
+
+    /**
+     * Display list table of items.
+     */
+    function list(Request $request)
+    {
+        $kategori = KategoriModel::select(['kategori_id', 'kategori_kode', 'kategori_nama']);
+
+        return DataTables::of($kategori)
+            ->addIndexColumn()
+            ->addColumn('aksi', function ($kategori) {
+                $btn = '<a href="' . url('/kategori/' . $kategori->kategori_id) . '" class="btn btn-info btn-sm mx-2">Detail</a> ';
+                $btn .= '<a href="' . url('/kategori/' . $kategori->kategori_id . '/edit') . '" class="btn btn-warning btn-sm mx-2">Edit</a> ';
+                $btn .= '<form class="d-inline-block mx-2" method="POST" action="' .
+                    url('/kategori/' . $kategori->kategori_id) . '">'
+                    . csrf_field() . method_field('DELETE') .
+                    '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakit menghapus data ini?\');">Hapus</button></form>';
+                return $btn;
+            })
+            ->rawColumns(['aksi'])
+            ->editColumn('kategori_id', '')
+            ->make(true);
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $breadcrumb = (object)[
+            'title' => 'Tambah Kategori',
+            'list' => ['Home', 'Kategori', 'Tambah']
+        ];
+
+        $page = (object)[
+            'title' => 'Tambah kategori baru'
+        ];
+
+        $activeMenu = 'kategori';
+
+        return view('kategori.create', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'kategori_kode' => 'required|string|min:4|unique:m_kategori,kategori_kode',
+            'kategori_nama' => 'required|string|max:100',
+        ]);
+
+        KategoriModel::create($request->only(['kategori_kode', 'kategori_nama']));
+
+        return redirect('/kategori')->with('success', 'Data kategori berhasil disimpan');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $kategori = KategoriModel::find($id);
+
+        $breadcrumb = (object)[
+            'title' => 'Detail Kategori',
+            'list' => ['Home', 'Kategori', 'Detail']
+        ];
+
+        $page = (object)[
+            'title' => 'Detail kategori'
+        ];
+
+        $activeMenu = 'kategori';
+
+        return view('kategori.show', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu, 'kategori' => $kategori]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $kategori = KategoriModel::find($id);
+
+        $breadcrumb = (object)[
+            'title' => 'Edit Kategori',
+            'list' => ['Home', 'Kategori', 'Edit']
+        ];
+
+        $page = (object)[
+            'title' => 'Edit kategori'
+        ];
+
+        $activeMenu = 'kategori';
+
+        return view('kategori.edit', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu, 'kategori' => $kategori]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'kategori_kode' => 'string|min:3',
+            'kategori_nama' => 'string|max:100',
+        ]);
+
+        KategoriModel::find($id)->update($request->only(['kategori_kode', 'kategori_nama']));
+
+        return redirect('/kategori')->with('success', 'Data kategori berhasil diubah');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $kategori = KategoriModel::find($id);
+
+        if (!$kategori) {
+            return redirect('/kategori')->with('error', 'Data kategori tidak ditemukan');
+        }
+
+        try {
+            $kategori->delete();
+
+            return redirect('/kategori')->with('success', 'Data kategori berhasil dihapus');
+        } catch (QueryException $e) {
+            return redirect('/kategori')->with('error', 'Data kategori gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
+        }
     }
 }
