78bd7b3defc0e96362a1fd6239fc666823ca9895
sshazzaraa <161804393+sshazzaraa@users.noreply.github.com>
2024-05-01 16:13:56 +0700

    PWL 10

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,21 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable; // Menggunakan User dari Foundation\Auth\User
 
-class UserModel extends Model
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
 
-    protected $table ='m_user';
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    protected $table = 'm_user';
     protected $primaryKey = 'user_id';
+
     /**
-     * The attributes that are mass assignable
-     * 
+     * The attributes that are mass assignable.
+     *
      * @var array
      */
-    protected $fillable = ["level_id", 'username', 'nama', 'password'];
-    public function level(): BelongsTo {
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+    public function level()
+    {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
-}   
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\Register;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +15,5 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Define the register endpoint
+Route::post('/register', RegisterController::class)->name('register');
